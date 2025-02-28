6117f5abea3bc55c7023d2ac76a3d0886d7ce9c8
Dilyara <diliara.kavieva@estudiantat.upc.edu>
2025-02-28 17:37:58 +0100

    Update puzzle.rb

--- puzzle.rb
+++ puzzle.rb
@@ -7,6 +7,9 @@ decimal_value = input.to_i  # Convierte la entrada a un número entero decimal
 hex_input = decimal_value.to_s(16).upcase.rjust(8, '0')  # Convierte a hexadecimal y asegura 8 caracteres
 hex_input = hex_input.scan(/.{2}/).reverse.join  # Invierte el orden de los bytes
 
+# se puede también escribir en una linia: 
+# hex_input = input.to_i.to_s(16).upcase.rjust(8, '0').scan(/.{2}/).reverse.join
+
 # Imprime la información leída y la representación hexadecimal
 puts "Información leída desde el lector (o teclado): #{input}"
 puts "Información en hexadecimal: #{hex_input}"
